25c1a2c811475558fcfcb95a784717f578bd1aca
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-02-18 17:46:55 +0100

    Initial version.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 railscasts-episodes
 ===================
 
-Ruby script to sync Railscasts.com repositories locally
+Ruby script to sync Railscasts.com repositories locally based on https://gist.github.com/ryanb/2866591
+
+Usage
+===================
+
+git clone https://github.com/Applicat/railscasts-episodes.git
+cd railscasts-episodes
+bundle install
+ruby sync_repositories.rb # clones every repository from github.com/railscasts/* which have not been cloned locally yet
---
+++ sync_repositories.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'octokit'
+
+page = 1
+
+catch(:done) do
+  while (repositories = Octokit.repositories('railscasts', page: page, per_page: 100, sorted: 'created', direction: 'desc')).length > 0
+    repositories.each do |repository|
+      path = "repositories/#{repository.name}"
+      
+      throw :done if File.exist? path
+      
+      system "git clone git://github.com/railscasts/#{repository.name} #{path}"
+    end
+    
+    page += 1
+  end
+end
+
+puts "Done."
